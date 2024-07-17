5f31359bc592378985c66eaf06e936fad8d5df90
Mitchell-610 <Mheit602@gmail.com>
2024-07-16 21:47:15 -0400

    Now we have taken in our license's within the prompt.

--- index.js
+++ index.js
@@ -81,4 +81,26 @@ function promptUser() {
             message: 'What about your test instructions?',
         },
 
+        {
+            type: 'list',
+            name: 'licenses',
+            message: 'Choose a license',
+            choices: [
+                "Apache License 2.0", 
+                "Eclipse Public License 1.0",
+                "MIT License", 
+                "BSD 2-Clause License",
+                "BSD 3-Clause License", 
+                "Boost Software License 1.0", 
+                "Creative Commons Zero v1.0 Universal", 
+                "GNU General Pulic License v3.0",
+                "GNU Affero General Public License v3.0",
+                "GNU General Public License v2.0", 
+                "GNU Lesser General Public License v2.1",
+                "Mozilla Public License 2.0", 
+                "The Unlicense"
+            ]
+        },
+
+        
     ])};
